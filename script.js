e366964a228fea13a44a42babfec4d30d25ec4f1
pau <paugitonga6@gmail.com>
2022-06-24 14:19:24 +0300

    done

--- script.js
+++ script.js
@@ -1,5 +1,57 @@
-const iconElement=document.querySelector()
-const tempElement=document.querySelector()
-const descElement=document.querySelector()
-const locationElement=document.querySelector()
-const notificationElement=document.querySelector()
+//select Elements
+//my api key=3e634834ad192fb4a392c07720892283
+
+const iconElement=document.querySelector('.weather-icon')
+const tempElement=document.querySelector('.temperature-value p')
+const descElement=document.querySelector('.temperature-description')
+const locationElement=document.querySelector('.location p')
+const notificationElement=document.querySelector('.notification')
+
+//data
+const weather={};
+weather.temperature={
+    unit:'celsius'
+};
+
+//const and variables
+const KELVIN=273;
+//API key 
+const key='3e634834ad192fb4a392c07720892283'
+
+//check i the broser supports geolocoalization
+if('geolocation'in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else
+   {
+    notificationElement.style.display='block';
+    notificationElement.innerHTML='<p>Broser doesn"t support Geolocalization'
+}
+
+//set user position
+function setPosition(position)
+{
+   let latitude = position.coords.latitude;
+   let longitude= position.coords.longitude;
+
+  getWeather(latitude,longitude);
+}
+
+//sho error hen ther is a problem ith geolocalization
+
+function showError(error)
+{
+    notificationElement.style.display='block';
+    notificationElement.innerHTML=`<p> ${error.message}`
+}
+
+//get api rom provider
+function getWeather(latitude,longitude){
+    let api =`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
+    fetch(api).then(function(response){
+        let data=response.json();
+        return data
+    });
+    .then(function(data{
+        weather.temperature.value=Math.floor(data.main.temp)
+    }))
+}
